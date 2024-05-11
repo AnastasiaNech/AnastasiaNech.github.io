c9e5921ca1f94d6fa3d769cd2afdbbdb6bf8b769
AnastasiaNech <nechaevaanastasiamih@gmail.com>
2024-05-11 17:33:29 +0300

    add singUp/In form

---
+++ src/pages/authPage/authPage.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+import { Title } from 'src/shared/forms/title/title';
+import { SingInForm } from './singInForm/singInForm';
+import { Link } from 'react-router-dom';
+
+export const AuthScreen: FC = () => {
+    return (
+        <div className="block">
+            <div>
+                <Title>{'Приложение для контроля доходов/расходов'}</Title>
+                <Link to="/regustration">{'Зарегистрироваться'}</Link>
+            </div>
+            <div className='vertical'></div>
+            <SingInForm />
+        </div>
+    );
+};
